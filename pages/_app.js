a3c3d8def311b3cff985afb596eda15cc9ff8bed
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-07 12:51:55 +0530

    initial tailblocks design

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
+import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import Footer from '@/components/footer'
 
 export default function App ({ Component, pageProps }) {
   return (
@@ -8,7 +10,9 @@ export default function App ({ Component, pageProps }) {
     <title>DevKart - e-com for devs</title>
         <meta name='description' content='Your page description' />
     </Head>
+    <Navbar/>
       <Component {...pageProps} />
+    <Footer/>
     </>
   )
 }
