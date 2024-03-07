6d9acffd98bca772dd303c9c5d0a66981086b157
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-07 18:52:38 +0530

    tshirts page defined

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -6,11 +7,15 @@ export default function Home() {
   return (
     <main>
       <div>
+      <div className="mt-5 flex justify-center items-center">
+  <Image src="/coder.png" alt="pixabay/coder.png" width={1920} height={100} />
+</div>
+
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
-      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">&lt;/devkart&gt;</h1>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Wear the code if you love the code: Unleash your passion with every stitch, let your attire speak the language of innovation.</p>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="xl:w-1/3 md:w-1/2 p-4">
