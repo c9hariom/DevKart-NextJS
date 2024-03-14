aaa7e5f9077dbd09a09bc66e9d121b701825ad32
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-14 19:02:32 +0530

    added test login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,10 +1,28 @@
-import connectDb from "@/middleware/mongoose"
+import connectDb from '@/middleware/mongoose'
+import User from '@/models/User'
 
-const handler = async (req,res)=>{
+const handler = async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const { password, email } = req.body
 
-    
+      const user = await User.findOne({ email })
 
+      if (!user) {
+        return res.status(404).json({ status: 'Wrong credentials' })
+      }
 
+      if (password === user.password) {
+        res.status(200).json(user)
+      } else {
+        res.status(404).json({ status: 'Wrong credentials' })
+      }
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(401).json({ status: 'this method is not supported' })
+  }
 }
 
 export default handler
