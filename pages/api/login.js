df3a814031c947a3e6d9716c3cc03908598aec37
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-03-14 20:04:03 +0530

    basic login api and functionality added

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,17 +2,22 @@ import connectDb from '@/middleware/mongoose'
 import User from '@/models/User'
 
 const handler = async (req, res) => {
-  if (req.method === 'GET') {
+  if (req.method === 'POST') {
     try {
-      const { password, email } = req.body
+      connectDb()
 
-      const user = await User.findOne({ email })
+      const { email, password } = req.body
+
+      const user = await User.findOne(
+        { email, password },
+        { password: 0, category: 0 }
+      )
 
       if (!user) {
         return res.status(404).json({ status: 'Wrong credentials' })
       }
 
-      if (password === user.password) {
+      if (email === user.email) {
         res.status(200).json(user)
       } else {
         res.status(404).json({ status: 'Wrong credentials' })
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,41 +1,134 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+  const [auth, setAuth] = useState({
+    email: '',
+    password: ''
+  })
+
+  const changeHandler = e => {
+    const { name, value } = e.target
+    setAuth(prevAuth => ({
+      ...prevAuth,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const { email, password } = auth
+    const response = await fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(auth)
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+    if(data.email === auth.email){
+        toast('Hi '+data.name+' good to see you back', {
+            position: 'top-left',
+            autoClose: 1200,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+            type: 'success'
+          })
+    } else{
+        toast('Oops '+data.status, {
+            position: 'top-left',
+            autoClose: 1200,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'light',
+            type: 'error'
+          })
+    }
+
+  }
+
   return (
-  <>
-  <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 mt-10">
-  <div className="px-6 py-4">
+    <>
+      <div className='w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 mt-10'>
+        <div className='px-6 py-4'>
+          <h3 className='mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200'>
+            <span className='ml-3 text-xl'>&lt;/devkart&gt;</span>
+          </h3>
 
-      <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200"><span className='ml-3 text-xl'>&lt;/devkart&gt;</span></h3>
+          <p className='mt-1 text-center text-gray-500 dark:text-gray-400'>
+            Login or create account
+          </p>
 
-      <p className="mt-1 text-center text-gray-500 dark:text-gray-400">Login or create account</p>
+          <form>
+            <div className='w-full mt-4'>
+              <input
+                className='block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
+                type='email'
+                placeholder='Email Address'
+                aria-label='Email Address'
+                onChange={changeHandler}
+                name='email'
+                required
+              />
+            </div>
 
-      <form>
-          <div className="w-full mt-4">
-              <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
-          </div>
+            <div className='w-full mt-4'>
+              <input
+                className='block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
+                type='password'
+                placeholder='Password'
+                aria-label='Password'
+                onChange={changeHandler}
+                name='password'
+                required
+                minLength={4}
+              />
+            </div>
 
-          <div className="w-full mt-4">
-              <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
-          </div>
+            <div className='flex items-center justify-between mt-4'>
+              <Link
+                href='/auth/forgot'
+                className='text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500'
+              >
+                Forget Password?
+              </Link>
 
-          <div className="flex items-center justify-between mt-4">
-              <Link href="/auth/forgot" className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500">Forget Password?</Link>
-
-              <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-500 rounded-lg hover:bg-red-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                  Sign In
+              <button
+                className='px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-500 rounded-lg hover:bg-red-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50'
+                type='submit'
+                onClick={handleSubmit}
+              >
+                Sign In
               </button>
-          </div>
-      </form>
-  </div>
+            </div>
+          </form>
+        </div>
 
-  <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
-      <span className="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
+        <div className='flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700'>
+          <span className='text-sm text-gray-600 dark:text-gray-200'>
+            Don't have an account?{' '}
+          </span>
 
-      <Link href="/auth/signup" className="mx-2 text-sm font-bold text-red-500 dark:text-blue-400 hover:underline">Register</Link>
-  </div>
-</div></>
+          <Link
+            href='/auth/signup'
+            className='mx-2 text-sm font-bold text-red-500 dark:text-blue-400 hover:underline'
+          >
+            Register
+          </Link>
+        </div>
+      </div>
+    </>
   )
 }
 
